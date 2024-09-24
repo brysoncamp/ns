ee10e06f3fffd07fd8a803fc1f142eff24502600
Bryson Camp <bryson@bryson.camp>
2024-09-24 14:55:29 +1000

    Updates

---
+++ server/entries/src_pages_index.mjs
@@ -0,0 +1,86 @@
+import import1 from "vike-react/components/Loading";
+import { onRenderHtml } from "vike-react/renderer/onRenderHtml";
+import { i as import3 } from "../chunks/chunk-DokNbUyK.js";
+import { jsxs, Fragment, jsx } from "react/jsx-runtime";
+import "react";
+/* empty css                       */
+/* empty css                       */
+function Page() {
+  return /* @__PURE__ */ jsxs(Fragment, { children: [
+    /* @__PURE__ */ jsxs("div", { className: "landing", children: [
+      /* @__PURE__ */ jsxs("h1", { className: "landing-hook", children: [
+        "Find your perfect ",
+        /* @__PURE__ */ jsx("br", {}),
+        "domain",
+        /* @__PURE__ */ jsx("span", { className: "boxed-text", children: /* @__PURE__ */ jsx("span", { className: "highlight-text", children: "in seconds" }) })
+      ] }),
+      /* @__PURE__ */ jsx("h2", { className: "landing-description", children: "Our AI rapidly searches thousands of domain names to help you secure the best available option." })
+    ] }),
+    /* @__PURE__ */ jsx("div", { style: { height: "2000px" } })
+  ] });
+}
+const import4 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  Page
+}, Symbol.toStringTag, { value: "Module" }));
+const configValuesSerialized = {
+  ["isClientRuntimeLoaded"]: {
+    type: "computed",
+    definedAtData: null,
+    valueSerialized: {
+      type: "js-serialized",
+      value: true
+    }
+  },
+  ["Loading"]: {
+    type: "standard",
+    definedAtData: { "filePathToShowToUser": "vike-react/components/Loading", "fileExportPathToShowToUser": [] },
+    valueSerialized: {
+      type: "pointer-import",
+      value: import1
+    }
+  },
+  ["passToClient"]: {
+    type: "cumulative",
+    definedAtData: [{ "filePathToShowToUser": "vike-react/config", "fileExportPathToShowToUser": ["default", "passToClient"] }],
+    valueSerialized: [{
+      type: "js-serialized",
+      value: []
+    }]
+  },
+  ["onRenderHtml"]: {
+    type: "standard",
+    definedAtData: { "filePathToShowToUser": "vike-react/renderer/onRenderHtml", "fileExportPathToShowToUser": [] },
+    valueSerialized: {
+      type: "pointer-import",
+      value: onRenderHtml
+    }
+  },
+  ["Layout"]: {
+    type: "cumulative",
+    definedAtData: [{ "filePathToShowToUser": "/src/pages/+Layout.jsx", "fileExportPathToShowToUser": [] }],
+    valueSerialized: [{
+      type: "plus-file",
+      exportValues: import3
+    }]
+  },
+  ["favicon"]: {
+    type: "standard",
+    definedAtData: { "filePathToShowToUser": "/src/pages/+config.js", "fileExportPathToShowToUser": ["default", "favicon"] },
+    valueSerialized: {
+      type: "js-serialized",
+      value: "/vite.svg"
+    }
+  },
+  ["Page"]: {
+    type: "standard",
+    definedAtData: { "filePathToShowToUser": "/src/pages/index/+Page.jsx", "fileExportPathToShowToUser": [] },
+    valueSerialized: {
+      type: "plus-file",
+      exportValues: import4
+    }
+  }
+};
+export {
+  configValuesSerialized
+};
